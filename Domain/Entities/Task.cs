9e7e3a9f49a143cf765d7879fa5bb132ee8f8b21
Bledian Potera <bledianpotera@outlook.com>
2026-02-17 13:56:49 +0100

    add Task, User Entity

---
+++ Domain/Entities/Task.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; private set; } = null!;
+        public string Description { get; private set; } = null!;
+        public Enums.TaskStatus Status { get; private set; } = Enums.TaskStatus.Draft;
+        public TaskPriority Priority { get; private set; } = TaskPriority.Medium;
+        public int UserId { get; private set; }
+        public User User { get; private set; } = null!;
+        public Task(string title, string description, int userId, TaskPriority priority = TaskPriority.Medium)
+        {
+            Title = title;
+            Description = description;
+            UserId = userId;
+            Priority = priority;
+            Status = Enums.TaskStatus.Draft;
+        }
+    }
+}
---
+++ Domain/Enums/TaskStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Enums
+{
+    public enum TaskStatus
+    {
+        Draft = 0,
+        InProgress = 1,
+        Completed = 2
+    }
+}
