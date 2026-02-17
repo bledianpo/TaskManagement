79b584b4f2fed832eb4c21199a63947decaa4805
Bledian Potera <bledianpotera@outlook.com>
2026-02-17 14:05:47 +0100

    add task respository interface

---
+++ Application/Interfaces/ITaskRepository.cs
@@ -0,0 +1,14 @@
+﻿using TaskEntity = Domain.Entities.Task;
+
+namespace Application.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task AddAsync(TaskEntity task);
+        Task<TaskEntity?> GetByIdAsync(int id);
+        Task<List<TaskEntity>> GetAllAsync();
+        Task<List<TaskEntity>> GetByUserIdAsync(int userId);
+        Task UpdateAsync(TaskEntity task);
+        Task DeleteAsync(TaskEntity task);
+    }
+}
