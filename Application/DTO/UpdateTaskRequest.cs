61b7e2e497daffcd521cd5b6e5b8524acf4b5e94
Bledian Potera <bledianpotera@outlook.com>
2026-02-18 23:23:49 +0100

    Use string for Priority/Status in task DTOs

--- Application/DTO/UpdateTaskRequest.cs
+++ Application/DTO/UpdateTaskRequest.cs
@@ -1,13 +1,10 @@
-﻿using Domain.Enums;
-using TaskStatus = Domain.Enums.TaskStatus;
-
 namespace Application.DTO
 {
     public class UpdateTaskRequest
     {
         public string? Title { get; set; }
         public string? Description { get; set; }
-        public TaskPriority? Priority { get; set; }
-        public TaskStatus? Status { get; set; }
+        public string? Priority { get; set; }
+        public string? Status { get; set; }
     }
 }
